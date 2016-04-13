bbe90613e7b53038782aef6284da20748a24b1a4
Chinakrit Pipatupatum <toon@prontomarketing.com>
2016-04-13 11:56:41 +0700

    Fix following variety of button style (.button, .btn)

--- custom-shop.js
+++ custom-shop.js
@@ -1,9 +1,12 @@
 jQuery(document).ready(function(){
     
     $('.woocommerce ul.products li.product').each(function(){
-        $(this).find('img').wrap('<div class="img-wrapper"></div>');
-        $(this).find('a.button').appendTo($(this).find('.img-wrapper'));
+        if($(this).find('a.button').length > 0){
+            $(this).find('a.button').appendTo($(this).find('.img-wrapper')).addClass('btn');
+        }else if($(this).find('a.btn').length > 0){
+            $(this).find('a.btn').appendTo($(this).find('.img-wrapper'));
+        }
     });
     
-    $('.woocommerce ul.products li.product a > .img-wrapper > a, .woocommerce-page ul.products li.product a .img-wrapper > a').addClass('btn');
+    //$('.woocommerce ul.products li.product a > .img-wrapper > a, .woocommerce-page ul.products li.product a .img-wrapper > a').addClass('btn');
 });
